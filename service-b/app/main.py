9d7b5614c5fb23e3a3b219f603bd7b5a5b800b81
shalom berman 7 <bermanshalom7@gmail.com>
2026-01-13 17:52:00 +0200

    order and organization

--- service-b/app/main.py
+++ service-b/app/main.py
@@ -2,16 +2,10 @@ from fastapi import FastAPI
 from app.routes import router1 as endpoints
 from app.routes import router2 as redis
 
-
-
 app = FastAPI(
     title="service B",
     version="1.0.0"
 )
 
-
-app.include_router(endpoints) 
-app.include_router(redis) 
-
-
-
+app.include_router(endpoints)
+app.include_router(redis)
--- service-b/app/routes.py
+++ service-b/app/routes.py
@@ -4,31 +4,30 @@ from app.storage import get_coord, post_coord
 router1 = APIRouter(tags=["service A"])
 router2 = APIRouter(tags=["Redis DB"])
 
+
 @router1.post("/connection")
 def get_ip():
     return {"status": "success"}
+
 
 @router2.get("/get_coord")
 def get_coordinates():
     return get_coord()
 
+
 @router2.post("/add_coord")
 async def add_coord(request: Request):
     data = await request.json()
-    
+
     if isinstance(data, str):
         import json
         data = json.loads(data)
-    
+
     ip = list(data.keys())[0]
     coord = list(data.values())[0]
-    
+
     try:
         post_coord(ip, coord)
         return {"message": "ip has been added successfully"}
     except Exception as e:
         return {"error": str(e)}
-
-
-
-
