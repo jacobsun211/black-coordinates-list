dc698ea107a1637362000db5e833a3687584c9ac
shalom berman 7 <bermanshalom7@gmail.com>
2026-01-13 12:49:03 +0200

    fix: update app

--- service-a/app/main.py
+++ service-a/app/main.py
@@ -1,19 +1,12 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from routes import router
+import routes
 import uvicorn
 
+
 app = FastAPI()
-
-
-@app.get('/api/get-ip')
-def get_ip():
-    pass
-
-
-@app.post('/api/ip')
-def create_coordinates():
-    pass
+app.include_router(routes.router)
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host='localhost', port=8000)
