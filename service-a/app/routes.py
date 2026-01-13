150c48d86dd0ea18f05eae3556dbfd89c40b08ca
shalom berman 7 <bermanshalom7@gmail.com>
2026-01-13 12:54:53 +0200

    fix: added router

---
+++ service-a/app/routes.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from services import get_IP_data
+from schemas import IPRequest, IPResponse
+
+
+
+router = APIRouter()
+
+
+@router.post('/ip')
+def create_ip(ip):
+    return get_IP_data(ip)
+
+
+@router.post('/connection')
+def send_coordinates_ip(ip_data):
+    return ip_data['ip']
